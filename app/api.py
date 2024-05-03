a496cbf5d5f2a61ff87c233d1a9b5583f20db2d0
Steffen Albrecht <steffen.alb@posteo.de>
2024-05-03 11:55:26 +0200

    change root endpoint and fixed docstring

--- app/api.py
+++ app/api.py
@@ -6,26 +6,24 @@ app = FastAPI()
 
 
 @app.get("/")
-def alive() -> dict:
+def alive() -> str:
     """Endpoint to check if the application is alive."""
-    return {"status": "alive"}
+    return "dwd-api is running."
 
 
 @app.post("/dwd/{var}")
 def get_forecast_values_for_locations(var: str, points: Union[RoutePoint, List[RoutePoint]]) -> List[float]:
     """Retrieve forecast values for a variable at specified locations along a route.
 
-    Args:
-        var (str): The variable to retrieve values for.
-        points (Union[RoutePoint, List[RoutePoint]]): Either a single RoutePoint or a list of RoutePoint objects
+        - var (str): The variable to retrieve values for.
+        - points: Either a single RoutePoint or a list of RoutePoint objects
         representing the route.
-        Each RoutePoint should be a JSON object with the following keys:
-        - lat: float
-        - lon: float
-        - time: str (ISO 8601 format)
+            - Each RoutePoint should be a JSON object with the following keys:
+                - lat: float
+                - lon: float
+                - time: str (ISO 8601 format)
 
-    Returns:
-        List[float]: List of forecast values corresponding to each point on the route.
+        - returns list of forecast values as float corresponding to each point on the route.
     """
     if isinstance(points, RoutePoint):
         points = [points]
